d9f4a3008889125f38b6cf33757fe232fe24bca8
Louis Mutricy <louis.mutricy@polytechnique.edu>
2021-10-15 10:19:07 +0200

    add bootstrap form design (experimental)

--- user_extra.php
+++ user_extra.php
@@ -163,7 +163,7 @@ if(isset($_REQUEST["action"]))
 <!DOCTYPE html>
 <html>
 
-<  <link rel="stylesheet" type="text/css" href="css/bootstrap/bootstrap.min.css" media="screen" />
+ <link rel="stylesheet" type="text/css" href="css/bootstrap/bootstrap.min.css" media="screen" />
    <link rel="stylesheet" type="text/css" href="css/main.css" media="screen" />
 
    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
@@ -188,14 +188,14 @@ if(isset($_REQUEST["action"]))
      <div class="collapse navbar-collapse" id="navbarColor02">
        <ul class="navbar-nav me-auto">
          <li class="nav-item">
-           <a class="nav-item" href="portal.php">Home
+           <a class="nav-link" href="portal.php">Home
            </a>
          </li>
          <li class="nav-item">
            <a class="nav-link"  href="password_change.php">Change Password</a>
             </li>
          <li class="nav-item">
-           <a class="active nav-link"  href="#">Create User</a>
+           <a class="nav-link active"  href="#">Create User</a>
              <span class="visually-hidden">(current)</span>
 
          </li>
@@ -221,6 +221,112 @@ if(isset($_REQUEST["action"]))
     <h1>Create User</h1>
 
     <p>Create an extra user.</p>
+
+    <form>
+      <fieldset>
+        <legend>Legend</legend>
+        <div class="form-group row">
+          <label for="staticEmail" class="col-sm-2 col-form-label">Email</label>
+          <div class="col-sm-10">
+            <input type="text" readonly="" class="form-control-plaintext" id="staticEmail" value="email@example.com">
+          </div>
+        </div>
+        <div class="form-group">
+          <label for="exampleInputEmail1" class="form-label mt-4">Email address</label>
+          <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email">
+          <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>
+        </div>
+        <div class="form-group">
+          <label for="exampleInputPassword1" class="form-label mt-4">Password</label>
+          <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password">
+        </div>
+        <div class="form-group">
+          <label for="exampleSelect1" class="form-label mt-4">Example select</label>
+          <select class="form-select" id="exampleSelect1">
+            <option>1</option>
+            <option>2</option>
+            <option>3</option>
+            <option>4</option>
+            <option>5</option>
+          </select>
+        </div>
+        <div class="form-group">
+          <label for="exampleSelect2" class="form-label mt-4">Example multiple select</label>
+          <select multiple="" class="form-select" id="exampleSelect2">
+            <option>1</option>
+            <option>2</option>
+            <option>3</option>
+            <option>4</option>
+            <option>5</option>
+          </select>
+        </div>
+        <div class="form-group">
+          <label for="exampleTextarea" class="form-label mt-4">Example textarea</label>
+          <textarea class="form-control" id="exampleTextarea" rows="3"></textarea>
+        </div>
+        <div class="form-group">
+          <label for="formFile" class="form-label mt-4">Default file input example</label>
+          <input class="form-control" type="file" id="formFile">
+        </div>
+        <fieldset class="form-group">
+          <legend class="mt-4">Radio buttons</legend>
+          <div class="form-check">
+            <label class="form-check-label">
+              <input type="radio" class="form-check-input" name="optionsRadios" id="optionsRadios1" value="option1" checked="">
+              Option one is this and that—be sure to include why it's great
+            </label>
+          </div>
+          <div class="form-check">
+            <label class="form-check-label">
+              <input type="radio" class="form-check-input" name="optionsRadios" id="optionsRadios2" value="option2">
+              Option two can be something else and selecting it will deselect option one
+            </label>
+          </div>
+          <div class="form-check disabled">
+            <label class="form-check-label">
+              <input type="radio" class="form-check-input" name="optionsRadios" id="optionsRadios3" value="option3" disabled="">
+              Option three is disabled
+            </label>
+          </div>
+        </fieldset>
+        <fieldset class="form-group">
+          <legend class="mt-4">Checkboxes</legend>
+          <div class="form-check">
+            <input class="form-check-input" type="checkbox" value="" id="flexCheckDefault">
+            <label class="form-check-label" for="flexCheckDefault">
+              Default checkbox
+            </label>
+          </div>
+          <div class="form-check">
+            <input class="form-check-input" type="checkbox" value="" id="flexCheckChecked" checked="">
+            <label class="form-check-label" for="flexCheckChecked">
+              Checked checkbox
+            </label>
+          </div>
+        </fieldset>
+        <fieldset>
+          <legend class="mt-4">Switches</legend>
+          <div class="form-check form-switch">
+            <input class="form-check-input" type="checkbox" id="flexSwitchCheckDefault">
+            <label class="form-check-label" for="flexSwitchCheckDefault">Default switch checkbox input</label>
+          </div>
+          <div class="form-check form-switch">
+            <input class="form-check-input" type="checkbox" id="flexSwitchCheckChecked" checked="">
+            <label class="form-check-label" for="flexSwitchCheckChecked">Checked switch checkbox input</label>
+          </div>
+        </fieldset>
+        <fieldset class="form-group">
+          <legend class="mt-4">Ranges</legend>
+            <label for="customRange1" class="form-label">Example range</label>
+            <input type="range" class="form-range" id="customRange1">
+            <label for="disabledRange" class="form-label">Disabled range</label>
+            <input type="range" class="form-range" id="disabledRange" disabled="">
+            <label for="customRange3" class="form-label">Example range</label>
+            <input type="range" class="form-range" min="0" max="5" step="0.5" id="customRange3">
+        </fieldset>
+        <button type="submit" class="btn btn-primary">Submit</button>
+      </fieldset>
+    </form>
 
     <form action="<?php echo($_SERVER["SCRIPT_NAME"]);?>" method="POST">
 
