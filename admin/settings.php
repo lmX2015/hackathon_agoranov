c20be78c3a9412ae72c81d566cc321fd1ff7c79d
Louis Mutricy <louis.mutricy@polytechnique.edu>
2022-11-27 19:21:19 +0100

    easier db access

--- admin/settings.php
+++ admin/settings.php
@@ -1,12 +1,12 @@
 <?php
 // Database connection settings
 $db_server = "localhost";
-$db_username = "xxx"; // replace this with db user once code is uploaded on the server
-$db_password = "xxx";  /// replace this with db password once code is uploaded on the server
-$db_name = "bWAPP";
+$db_username = "admin"; // replace this with db user once code is uploaded on the server
+$db_password = "admin";  /// replace this with db password once code is uploaded on the server
+$db_name = "hackathon";
 
 // SQLite database name
-$db_sqlite = "db/bwapp.sqlite";
+$db_sqlite = "db/hackathon.sqlite";
 
 // Static credentials
 // These credentials are used on some PHP pages
